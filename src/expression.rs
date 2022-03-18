18c2a5c99eba1dfd0ce3ee1836111969834d153e
Kyle Lin <minecraft.kyle.train@gmail.com>
2022-03-18 22:54:21 +0800

    derive common traits

--- src/expression.rs
+++ src/expression.rs
@@ -1,4 +1,4 @@
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Expression {
    pub lhs: String,
    pub rhs: crate::Node,
--- src/grammar.rs
+++ src/grammar.rs
@@ -1,5 +1,6 @@
 use crate::expression::Expression;
 
+#[derive(Debug, Clone)]
 pub struct Grammar {
     pub expressions: Vec<Expression>
 }
